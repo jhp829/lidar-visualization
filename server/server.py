33aed0e853c1fc21dbb88298dc6fbe31e9bf37dd
nataliewutc <nataliewu.261201@gmail.com>
2022-02-26 20:08:05 +0000

    added server files

---
+++ server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request, render_template, send_file, send_from_directory
+import os
+
+template_dir = os.path.abspath('../client')
+app = Flask(name, template_folder=template_dir)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
