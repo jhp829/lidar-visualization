061e77293504329915679b4688b277c3f3e93b7e
Robert Stok <rs.stok@gmail.com>
2022-02-26 14:04:44 +0000

    test point loaded in

--- components/point-cloud-loader.js
+++ components/point-cloud-loader.js
@@ -6,7 +6,12 @@ AFRAME.registerComponent('point-cloud-loader', {
   lidar_file_path: '../components/',
 
   init: function () {
-      
+    this.points.set(0, {
+      x: 1,
+      y: 1,
+      z: 1
+    })
+    this.add_point(0)
   },
 
   load_new_file: function(file_name) {
@@ -14,18 +19,21 @@ AFRAME.registerComponent('point-cloud-loader', {
   },
 
   add_point: function (point_index) {
-    var point = points[point_index]
+    var point = this.points.get(point_index)
 
     // create point element
     var point_element = document.createElement('a-sphere');
 
     // set attributes
     var position = { x: point.x, y: point.y, z: point.z }
-    new_product.setAttribute('position', position);
-    new_product.setAttribute('scale', { x: 0.05, y: 0.05, z: 0.05 });
-    new_product.setAttribute('color', '#F5E942');
+    point_element.setAttribute('position', position);
+    point_element.setAttribute('scale', { x: 0.05, y: 0.05, z: 0.05 });
+    point_element.setAttribute('color', '#F5E942');
 
     // append it to the scene
     this.el.appendChild(point_element)
+
+    // add it to the list of loaded points so it can be easily removed later
+    this.loaded_points.set(point_index, point_element)
   }
 });
