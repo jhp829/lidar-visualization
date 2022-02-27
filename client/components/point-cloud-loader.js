9a617928dcb65ea14b02863dd35114d21142f558
Robert Stok <rs.stok@gmail.com>
2022-02-27 01:08:52 +0000

    only surrounding blocks are loaded now

--- client/components/point-cloud-loader.js
+++ client/components/point-cloud-loader.js
@@ -2,73 +2,61 @@ AFRAME.registerComponent('point-cloud-loader', {
   schema: {},
 
   points: new Map(),
-  min_loaded_index: 0,
-  max_loaded_index: 0,
+  current_position: { x: 0, y: 0, z: 0 },
   loaded_points: new Map(),
   lidar_file_path: '../lidar_data/',
-  time_offset: 0,
-  window_size: 1000, // in milliseconds
+  view_radius: 10, // in meters
 
   init: function () {
     this.load_new_file('235.json')
-    this.el.addEventListener('abuttonup', function() {
-      this.time_offset += 200
+    this.el.addEventListener('new-position', function(position) {
+      this.current_position = position
       this.reload_points()
-    });
+    }, true);
   },
 
   load_new_file: function (filename) {
     // reset all tracking variables
     this.points = new Map()
     this.loaded_points = new Map()
-    this.min_loaded_index = 0
-    this.max_loaded_index = 0
-    this.time_offset = 0
 
     fetch(this.lidar_file_path + filename)
     .then(response => {
        return response.json();
     })
     .then(point_data => {
-      console.log(point_data)
-      console.log(typeof(point_data))
       // load points in
       for (let key in point_data.dataPoints) {
-        this.points.set(parseInt(key), point_data.dataPoints[key])
+        this.points.set(key, point_data.dataPoints[key])
       }
-      console.log(this.points)
       this.reload_points()
     })
   },
 
   reload_points: function () {
-    // unloads all 'old' points
-    // then loads all newly 'in timestamp' points
-    function minLoadedTimestamp(outer_this) {
-      if (outer_this.loaded_points.size == 0) {
-        return 100000000000
-      }
-      return outer_this.points.get(outer_this.min_loaded_index).timestamp
-    }
-    function maxLoadedTimestamp(outer_this) {
-      if (outer_this.loaded_points.size == 0) {
-        return 0
-      }
-      return outer_this.points.get(outer_this.max_loaded_index).timestamp
+    // remove all points
+    for (let point_index in this.loaded_points) {
+      this.remove_point(point_index)
     }
 
-    while(maxLoadedTimestamp(this) < this.time_offset + this.window_size) {
-      this.add_point(this.max_loaded_index + 1)
+    var outer_this = this
+    function add_square(x, y) {
+      var square_key = x.toString() + "," + y.toString()
+      for(point_index in outer_this.points.get(square_key)) {
+        outer_this.add_point(point_index, square_key)
+      }
     }
 
-    while(minLoadedTimestamp(this) < this.time_offset) {
-      this.remove_point(this.min_loaded_index)
+    for( x = -this.view_radius; x < this.view_radius; x++) {
+      for ( y = -this.view_radius; y < this.view_radius; y++) {
+        add_square(x, y)
+      }
     }
   },
 
   // we assume the point added has the highest index of all points
-  add_point: function (point_index) {
-    var point = this.points.get(point_index)
+  add_point: function (point_index, square_key) {
+    var point = this.points.get(square_key)[point_index]
 
     // create point element
     var point_element = document.createElement('a-box');
@@ -84,14 +72,33 @@ AFRAME.registerComponent('point-cloud-loader', {
 
     // add it to the list of loaded points so it can be easily removed later
     this.loaded_points.set(point_index, point_element)
-    this.max_loaded_index = point_index
   },
+
+  // // we assume the point added has the highest index of all points
+  // add_point: function (point_index) {
+  //   var point = this.points.get(point_index)
+
+  //   // create point element
+  //   var point_element = document.createElement('a-box');
+
+  //   // set attributes
+  //   var position = { x: point.x, y: point.y, z: point.z }
+  //   point_element.setAttribute('position', position);
+  //   point_element.setAttribute('scale', { x: 0.05, y: 0.05, z: 0.05 });
+  //   point_element.setAttribute('color', '#F5E942');
+
+  //   // append it to the scene
+  //   this.el.appendChild(point_element)
+
+  //   // add it to the list of loaded points so it can be easily removed later
+  //   this.loaded_points.set(point_index, point_element)
+  //   this.max_loaded_index = point_index
+  // },
 
   // we assume the point removed has the lowest index of all points
   remove_point: function (point_index) {
     var point_element = this.loaded_points.get(point_index)
     this.el.removeChild(point_element)
     this.loaded_points.delete(point_index)
-    this.min_loaded_index = point_index + 1
   }
 });
